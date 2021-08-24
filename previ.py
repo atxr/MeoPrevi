3c11416adab8c06e35d2d6c79f1653ecf49f3523
aTxr <alexandre.tullot@gmail.com>
2021-08-24 11:15:44 +0200

    Auto refresh for time, that doesn't change x_range

--- previ.py
+++ previ.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 # Grab the prevision of important satellites
 
-import os
-from plotly.express import timeline
-import plotly.graph_objects as go
+
 import pandas as pd
-from datetime import datetime, timedelta
+from plotly.express import timeline
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from datetime import datetime, timedelta
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -78,12 +76,18 @@ def previ(sat, satid):
 
 
 
-def get_figure(df):
+def get_figure(df, range_x=[]):
     utc = datetime.utcnow().replace(microsecond=0)
-    xmin = (utc - timedelta(minutes=10)).strftime("%Y-%m-%d %H:%M:%S")
-    xmax = (utc + timedelta(hours=2)).strftime("%Y-%m-%d %H:%M:%S")
+    if range_x== []:
+        range_x = [(utc - timedelta(minutes=10)).strftime("%Y-%m-%d %H:%M:%S"),
+                   (utc + timedelta(hours=2)).strftime("%Y-%m-%d %H:%M:%S")]
 
-    fig = timeline(df, x_start = 'Start', x_end='Finish', title="Previ MEO", y='Sat', color='Sat', range_x=[xmin,xmax], hover_name='Sat', height=900)
+    fig = timeline(
+            df, 
+            x_start = 'Start', x_end='Finish', 
+            title="Previ MEO", y='Sat', color='Sat', 
+            range_x=range_x, 
+            hover_name='Sat', height=900)
     fig.update_layout(
         hoverlabel=dict(
             font_size=25
@@ -91,9 +95,14 @@ def get_figure(df):
     )
     print(utc)
     fig.add_vline(x=utc, line_width=3, line_dash="dash")
-    return fig
+    return fig, range_x
 
 
+def get_df(sats):
+    df = pd.DataFrame([])
+    for sat in sats:
+        df = df.append(previ(sat, sats[sat])) 
+    return df
 
 
 
@@ -104,12 +113,8 @@ if __name__=='__main__':
     with open(database_sats, 'r') as f:
         sats = {sat:idsat for [sat, idsat] in [x.strip('\n').split('~') for x in f.readlines()]}
 
-    df = pd.DataFrame([])
-    for sat in sats:
-        df = df.append(previ(sat, sats[sat])) 
-
-    print(df)
-    fig = get_figure(df)
+    df = get_df(sats)
+    fig, range_x = get_figure(df)
 
     external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
     app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -119,28 +124,43 @@ if __name__=='__main__':
         dcc.Input(id='new_sat_id', type='text', placeholder='Satelitte ID'),
         html.Button('Add', id='add_button'),
         html.Button('Refresh', id='refresh_button'),
-        dcc.Graph(figure=fig, id='timeline')
+        dcc.Graph(figure=fig, id='timeline'), 
+        dcc.Interval(
+            id='interval-component',
+            interval=5*60*1000, # 5min (in milliseconds)
+            n_intervals=0
+        )
     ])
 
     @app.callback(
         dash.dependencies.Output('timeline', 'figure'),
         [dash.dependencies.Input('refresh_button', 'n_clicks')],
         [dash.dependencies.Input('add_button', 'n_clicks')],
+        [dash.dependencies.Input('interval-component', 'n_intervals')],
         [dash.dependencies.State('new_sat_name', 'value')],
         [dash.dependencies.State('new_sat_id', 'value')])
-    def update_output(n_ref, n_add, sat, satid):
+    def update_output(n_ref, n_add, n_inter, sat, satid):
         ctx = dash.callback_context
         if not ctx.triggered:
             raise dash.exceptions.PreventUpdate
         
         btn_id = ctx.triggered[0]['prop_id'].split('.')[0]
         global df
+        global range_x
         if btn_id == 'add_button':
+            #add new satellite to the figure, update time, but keep the range_x
             df = df.append(previ(sat, satid))
+            figure, range_x = get_figure(df, range_x=range_x)
+        elif btn_id == 'refresh_button':
+            #update time and range_x 
+            figure, range_x = get_figure(df)
+        else: #btn_id == 'interval-component'
+            #update time only every 5min
+            figure, range_x = get_figure(df, range_x=range_x)
 
-        return get_figure(df)
+        return figure
 
-
-    os.system("sleep 2; xdg-open http://127.0.0.1:8050/")
+    #import os
+    #os.system("sleep 2; xdg-open http://127.0.0.1:8050/")
     app.run_server(debug=True)
 
