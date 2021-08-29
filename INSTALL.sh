bcf57e999a7fd3a396ab9b4871abb3329e692198
aTxr <alexandre.tullot@gmail.com>
2021-08-29 09:06:41 +0200

    Correct ln bug install.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,5 +4,5 @@ pip3 install numpy plotly selenium pyautogui dash
 wget --output-document /tmp/geckodriver-v0.29.1-linux64.tar.gz \
   'https://github.com/mozilla/geckodriver/releases/download/v0.29.1/geckodriver-v0.29.1-linux64.tar.gz' 
 tar -xf /tmp/geckodriver-v0.29.1-linux64.tar.gz -C ~/.local/bin/MeoPrevi
-ln -s ~/.local/bin/previ ~/.local/bin/MeoPrevi/previ.py
-ln -s ~/.local/bin/geckodriver ~/.local/bin/MeoPrevi/geckodriver
+ln -s ~/.local/bin/MeoPrevi/previ.py ~/.local/bin/previ 
+ln -s ~/.local/bin/MeoPrevi/geckodriver ~/.local/bin/geckodriver 
